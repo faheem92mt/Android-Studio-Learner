c644271f23fb6aad72b75d156410d887b489be10
faheem92mt <faheemibnhabib@gmail.com>
2022-03-26 02:31:05 +0800

    Initial commit for the Top 10 Downloader App

---
+++ Top10Downloader/app/src/main/java/com/faheem92mt/MainActivity.java
@@ -0,0 +1,145 @@
+package com.faheem92mt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ListView listApps;
+    private TextView tvName;
+    private TextView tvArtist;
+    private TextView tvSummary;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_record);
+        listApps = (ListView) findViewById(R.id.xmlListView);
+
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvArtist = (TextView) findViewById(R.id.tvArtist);
+        tvSummary = (TextView) findViewById(R.id.tvSummary);
+
+        tvSummary.setMovementMethod(new ScrollingMovementMethod());
+
+        //
+        Log.d(TAG, "onCreate: starting Async task");
+        //
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml");
+
+        //
+        Log.d(TAG, "onCreate: done");
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        private static final String TAG = "DownloadData";
+        
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //
+            Log.d(TAG, "onPostExecute: parameter is " + s);
+            ParseApplications parseApplications = new ParseApplications();
+            parseApplications.parse(s);
+
+//            ArrayAdapter<FeedEntry> arrayAdapter = new ArrayAdapter<FeedEntry>(MainActivity.this, R.layout.list_item, parseApplications.getApplications());
+//            listApps.setAdapter(arrayAdapter);
+
+            ArrayList<FeedEntry> apps;
+            apps = parseApplications.getApplications();
+            String ss1 = apps.get(0).getName();
+            String ss2 = apps.get(0).getArtist();
+            String ss3 = apps.get(0).getSummary();
+
+            tvName.setText(ss1);
+            tvArtist.setText(ss2);
+            tvSummary.setText(ss3);
+
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            //
+            Log.d(TAG, "doInBackground: starts with " + strings[0]);
+
+            String rssFeed = downloadXML(strings[0]);
+            if(rssFeed == null) {
+                Log.e(TAG, "doInBackground: Error downloading");
+            }
+
+            //
+            return rssFeed;
+        }
+
+        private String downloadXML(String urlPath) {
+            StringBuilder xmlResult = new StringBuilder();
+
+            try {
+                URL url = new URL(urlPath);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // "response" stores the response code retrieved from the server when opening the http connection
+                int response = connection.getResponseCode();
+                Log.d(TAG, "downloadXML: The response was " + response);
+
+//                InputStream inputStream = connection.getInputStream();
+//                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//                BufferedReader reader = new BufferedReader(inputStreamReader);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                int charRead;
+                char[] inputBuffer = new char[500];
+
+                while (true) {
+                    charRead = reader.read(inputBuffer);
+//                    Log.d(TAG, "charRead is " + charRead);
+//                    System.out.println(charRead);
+                    if (charRead < 0) {
+                        break;
+                    }
+                    if (charRead > 0) {
+                        xmlResult.append(String.copyValueOf(inputBuffer, 0, charRead));
+                    }
+                }
+                reader.close();
+
+                return xmlResult.toString();
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "downloadXML: Invalid URL " + e.getMessage());
+            } catch (IOException e) {
+                Log.e(TAG, "downloadXML: IO Exception reading data: " + e.getMessage());
+            } catch (SecurityException e) {
+                Log.e(TAG, "downloadXML: Security Exception. Needs permission? " + e.getMessage());
+//                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+    }
+
+}
---
+++ Top10Downloader/app/src/main/java/com/faheem92mt/ParseApplications.java
@@ -0,0 +1,117 @@
+package com.faheem92mt;
+
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+
+public class ParseApplications {
+
+    private static final String TAG = "ParseApplication";
+    private ArrayList<FeedEntry> applications;
+
+//    public ParseApplications(ArrayList<FeedEntry> applications) {
+//        this.applications = applications;
+//    }
+
+
+    public ParseApplications() {
+        // initializes the array list
+        this.applications = new ArrayList<>();
+    }
+
+    public ArrayList<FeedEntry> getApplications() {
+        // to return the arraylist
+        return applications;
+    }
+
+    public boolean parse(String xmlData) {
+        // if method parse will return false if it fails to parse
+        boolean status =true;
+        // the latest app while parsing the xml gets stored in this variable first
+        FeedEntry currentRecord = null;
+        // this boolean value will tell if we're inside the entry tag of the app in question while we're parsing the xml (the entry tag here refers to the ones inside xml)
+        boolean inEntry = false;
+        // this will store the value of the current tag
+        String textValue = "";
+
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser xpp = factory.newPullParser();
+            // "xmlData" is a String which holds the xml stuff (downloaded from iTunes website) and its read to the pull parser object (xpp) using a StringReader
+            xpp.setInput(new StringReader(xmlData));
+
+            // this will help us check if we have reached the end of xml and also to iterate while within the xml
+            int eventType = xpp.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                // a string that tells what is the current tag which we're at
+                String tagName = xpp.getName();
+
+                switch (eventType) {
+                    // only 3 cases -> start or end of a tag and also another case to store text values within a tag
+
+                    // case 1
+                    case XmlPullParser.START_TAG:
+                        Log.d(TAG, "parse: Starting tag for " + tagName);
+
+                        // this if condition makes sure that only if we're inside an entry tag (which represents one app) we're going
+                        // to be creating a new stuff to be added to the arraylist, which will represent one particular app
+                        if ("entry".equalsIgnoreCase(tagName)) {
+                            inEntry = true;
+                            currentRecord = new FeedEntry();
+                        }
+
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        textValue = xpp.getText();
+                        break;
+
+                    // case 2
+                    case XmlPullParser.END_TAG:
+                        Log.d(TAG, "parse: Ending tag for " + tagName);
+                        if(inEntry) {
+                            if("entry".equalsIgnoreCase(tagName)) {
+                                applications.add(currentRecord);
+                                inEntry = false;
+                            } else if("name".equalsIgnoreCase(tagName)) {
+                                currentRecord.setName(textValue);
+                            } else if("artist".equalsIgnoreCase(tagName)) {
+                                currentRecord.setArtist(textValue);
+                            } else if("releaseDate".equalsIgnoreCase(tagName)) {
+                                currentRecord.setReleaseDate(textValue);
+                            } else if ("summary".equalsIgnoreCase(tagName)) {
+                                currentRecord.setSummary(textValue);
+                            } else if("image".equalsIgnoreCase(tagName)) {
+                                currentRecord.setImageURL(textValue);
+                            }
+                        }
+                        break;
+
+                    default:
+                        // do nothing
+                }
+                // jumps to the next tag
+                eventType = xpp.next();
+
+            }
+            for (FeedEntry app: applications) {
+                Log.d(TAG, "***********************");
+                Log.d(TAG, app.toString());
+            }
+
+        }
+        catch (Exception e) {
+            status = false;
+            e.printStackTrace();
+        }
+
+        return status;
+    }
+
+
+}
