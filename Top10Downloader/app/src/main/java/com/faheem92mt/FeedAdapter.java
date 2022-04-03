c22a1879a5ab8b16451a5c053fca5837eb306d43
faheem92mt <faheemibnhabib@gmail.com>
2022-04-03 23:49:49 +0800

    Added a menu to the mini-App

--- Top10Downloader/app/src/main/java/com/faheem92mt/FeedAdapter.java
+++ Top10Downloader/app/src/main/java/com/faheem92mt/FeedAdapter.java
@@ -34,21 +34,42 @@ public class FeedAdapter extends ArrayAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        ViewHolder viewHolder;
+
         if(convertView == null) {
             convertView = layoutInflater.inflate(layoutResource, parent, false);
+
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
-        TextView tvArtist = (TextView) convertView.findViewById(R.id.tvArtist);
-        TextView tvSummary = (TextView) convertView.findViewById(R.id.tvSummary);
+//        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+//        TextView tvArtist = (TextView) convertView.findViewById(R.id.tvArtist);
+//        TextView tvSummary = (TextView) convertView.findViewById(R.id.tvSummary);
 
         FeedEntry currentApp = applications.get(position);
 
-        tvName.setText(currentApp.getName());
-        tvArtist.setText(currentApp.getArtist());
-        tvSummary.setText(currentApp.getSummary());
+        viewHolder.tvName.setText(currentApp.getName());
+        viewHolder.tvArtist.setText(currentApp.getArtist());
+        viewHolder.tvSummary.setText(currentApp.getSummary());
 
         return convertView;
+    }
+
+    private class ViewHolder {
+        final TextView tvName;
+        final TextView tvArtist;
+        final TextView tvSummary;
+
+        ViewHolder(View v) {
+            this.tvName = v.findViewById(R.id.tvName);
+            this.tvArtist = v.findViewById(R.id.tvArtist);
+            this.tvSummary = v.findViewById(R.id.tvSummary);
+        }
+
     }
 
 }
--- Top10Downloader/app/src/main/java/com/faheem92mt/MainActivity.java
+++ Top10Downloader/app/src/main/java/com/faheem92mt/MainActivity.java
@@ -1,11 +1,14 @@
 package com.faheem92mt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -41,20 +44,56 @@ public class MainActivity extends AppCompatActivity {
 //
 //        tvSummary.setMovementMethod(new ScrollingMovementMethod());
 
+        downloadUrl("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.feeds_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        String feedUrl;
+
+        switch (id) {
+            case R.id.mnuFree:
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml";
+                break;
+            case R.id.mnuPaid:
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=10/xml";
+                break;
+            case R.id.mnuSongs:
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml";
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        downloadUrl(feedUrl);
+        return true;
+
+    }
+
+    private void downloadUrl(String feedUrl) {
         //
-        Log.d(TAG, "onCreate: starting Async task");
+        Log.d(TAG, "downloadUrl: starting Async task");
         //
         DownloadData downloadData = new DownloadData();
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+        downloadData.execute(feedUrl);
 
         //
-        Log.d(TAG, "onCreate: done");
+        Log.d(TAG, "downloadUrl: done");
     }
 
     private class DownloadData extends AsyncTask<String, Void, String> {
 
         private static final String TAG = "DownloadData";
-        
+
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
@@ -81,7 +120,6 @@ public class MainActivity extends AppCompatActivity {
 //            tvSummary.setText(ss3);
 
 
-
         }
 
         @Override
@@ -90,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "doInBackground: starts with " + strings[0]);
 
             String rssFeed = downloadXML(strings[0]);
-            if(rssFeed == null) {
+            if (rssFeed == null) {
                 Log.e(TAG, "doInBackground: Error downloading");
             }
 
